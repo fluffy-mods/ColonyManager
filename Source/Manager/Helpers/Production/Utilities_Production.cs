3e44fb74bb64eb1a2277cac9e58b7094cdf0373a
Karel Kroeze <karel.kroeze@gmail.com>
2015-12-22 09:46:20 +0100

    round number of bills per worker correctly. Resolves #10.

--- Source/Manager/Helpers/Production/Utilities_Production.cs
+++ Source/Manager/Helpers/Production/Utilities_Production.cs
@@ -42,54 +42,63 @@ namespace FluffyManager
         ///     Amount per worker to satsify the bill.
         /// </summary>
         /// <param name="job"></param>
-        /// <param name="i"></param>
+        /// <param name="workerIndex"></param>
         /// <returns></returns>
-        public static int CountPerWorker( this ManagerJob_Production job, int i )
+        public static int CountPerWorker( this ManagerJob_Production job, int workerIndex )
         {
             int n = job.BillGivers.CurBillGiverCount;
-            int diff = Mathf.CeilToInt( job.Trigger.Count - job.Trigger.CurCount );
+            int diff = job.Trigger.Count - job.Trigger.CurCount;
 
             int bills;
-            // if diff is negative this is a destructive job.
+
+            // if diff is negative we can assume this is a destructive job.
             if ( diff < 0 )
             {
                 // default input count set to 1
-                float count = 1;
-                var filterThingDefs = job.Trigger.ThresholdFilter.AllowedThingDefs;
+                float inputCount = 1;
+                IEnumerable<ThingDef> filterThingDefs = job.Trigger.ThresholdFilter.AllowedThingDefs;
 
                 // if the filter is a single thingdef, try to figure out what the input count of it is for the managed recipe.
                 if ( filterThingDefs?.Count() == 1 )
                 {
                     // only one thingdef in the filter, so get the first.
-                    var filterThingDef = filterThingDefs.First();
+                    ThingDef filterThingDef = filterThingDefs.First();
 
                     // the ingredient list of the managed recipe.
-                    var ingredients = job.Bill.recipe.ingredients;
+                    List<IngredientCount> ingredients = job.Bill.recipe.ingredients;
 
                     // get the total input count of any ingredients that allow our thingdef (probably 0 or 1, but who knows - could be a stuff + specific listing).
-                    float? recipeInput = ingredients?.Where( ing => ing.filter.AllowedThingDefs.Contains( filterThingDef ) ).Select( ing => ing.CountRequiredOfFor(filterThingDef, job.Bill.recipe) ).Sum();
+                    float? recipeInput =
+                        ingredients?.Where( ing => ing.filter.AllowedThingDefs.Contains( filterThingDef ) )
+                                    .Select( ing => ing.CountRequiredOfFor( filterThingDef, job.Bill.recipe ) )
+                                    .Sum();
 
                     // if this wasn't null or close to zero, set the reduction count per bill to this number.
-                    if ( recipeInput != null && Math.Abs( recipeInput.Value ) > 1 )
+                    if ( recipeInput != null &&
+                         Math.Abs( recipeInput.Value ) > 1 )
                     {
-                        count = recipeInput.Value;
+                        inputCount = recipeInput.Value;
                     }
                 }
 
                 // divide by negative reduction amount per bill to get a positive number of bills.
-                bills = Mathf.CeilToInt( diff / - count );
-            } else
+                bills = Mathf.CeilToInt( diff / - inputCount );
+            }
+            else
             {
                 // this one is a bit simpler - mainly because we already did the complicated stuff in the MainProduct class.
                 bills = Mathf.CeilToInt( diff / job.MainProduct.Count );
             }
-            
+
+            // naive number of bills per worker (float)
             float naive = bills / (float)n;
-            if ( bills % n > i )
+
+            // round up or down to get a total that matches the desired total exactly
+            if ( bills % n > workerIndex )
             {
-                return (int)Math.Ceiling( naive );
+                return (int)Math.Floor( naive );
             }
-            return (int)Math.Floor( naive );
+            return (int)Math.Ceiling( naive );
         }
 
         /// <summary>
