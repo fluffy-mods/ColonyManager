08242c8236e34619f8980897c3ecf8c97eccf606
Fluffy <fluffy.l2032@gmail.com>
2018-10-02 10:41:35 +0200

    'Release 3.22.398 [nolog]'

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -42,5 +42,5 @@ using Verse;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.21.397")]
-[assembly: AssemblyFileVersion("3.21.397")]
+[assembly: AssemblyVersion("3.22.398")]
+[assembly: AssemblyFileVersion("3.22.398")]
