6de641aa8936680692cc9e881cb8ba50b0eeac75
Fluffy <fluffy.l2032@gmail.com>
2019-01-23 23:51:55 +0100

    'Release 4.5.431 [nolog]'

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -43,4 +43,4 @@ using Verse;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.4.430")]
+[assembly: AssemblyFileVersion("4.5.431")]
