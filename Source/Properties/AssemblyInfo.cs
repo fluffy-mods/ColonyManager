20d10827f935699c0aa31fb0ec7a0d4cd9035f53
Fluffy <fluffy.l2032@gmail.com>
2019-08-04 01:06:26 +0200

    'Release 4.14.589 [nolog]'

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -40,4 +40,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.13.588")]
+[assembly: AssemblyFileVersion("4.14.589")]
