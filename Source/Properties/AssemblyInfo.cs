13616dc9f58aba6b5671744f30c6efc685af0d22
Fluffy <fluffy.l2032@gmail.com>
2020-03-07 21:36:21 +0100

    'Release 4.26.601 [nolog]'

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -40,4 +40,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.25.600")]
+[assembly: AssemblyFileVersion("4.26.601")]
