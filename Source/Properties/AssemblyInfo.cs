b3d6ee9c0becde94fbaab9681a9dd41dc93ef681
Fluffy <fluffy.l2032@gmail.com>
2020-12-08 19:02:42 +0100

    Release 4.33.652 [nolog]

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -39,4 +39,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.33.649")]
+[assembly: AssemblyFileVersion("4.33.652")]
